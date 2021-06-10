f5866ea4e57436e89e055492a5ddcd75b2d11b64
hud <hudwahab@gmail.com>
2021-06-10 08:17:24 -0600

    add config file

---
+++ config.py
@@ -0,0 +1,27 @@
+# config.py
+
+import os
+
+# Set a bot version
+CODE_VERSION = '0.6'
+
+# Database
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+db_path = os.path.join(BASE_DIR, 'tokens_testnet.db')
+SQLALCHEMY_DATABASE_URI = f'sqlite:///{db_path}'
+SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+# IPFS - blockfrost limited to 100mb, try nft-storage
+BLOCKFROST_IPFS = os.getenv('BLOCKFROST_IPFS')
+
+# NFTSTORAGE
+NFTSTORAGE = os.getenv('NFTSTORAGE')
+
+# Token
+TESTNET_ID = os.getenv('TESTNET_ID')
+CARDANO_CLI = "/Applications/Daedalus\ Testnet.app/Contents/MacOS/cardano-cli"
+BLOCKFROST_TESTNET = os.getenv('BLOCKFROST_TESTNET')
+
+# Misc
+# Set the buffer to 1 hour
+SLOT_CUSHION = 3600
