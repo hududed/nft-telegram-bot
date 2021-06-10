4007367a25c468e532b3337e4e72bb3f04c39ad9
hud <hudwahab@gmail.com>
2021-06-10 09:20:00 -0600

    ipfs check OK

--- ipfs_util.py
+++ ipfs_util.py
@@ -21,3 +21,22 @@ def create_ipfs(image):
         else:
             logger.error("Something failed here? Upload to blockfrost failed")
             return False
+
+def check_ipfs(ipfs_hash):
+    """ Check to see if the ipfs hash is accessible via
+    ipfs.io and cloudflare """
+    ipfs_gateways = [
+        f"https://gateway.ipfs.io/ipfs/{ipfs_hash}",
+        f"https://cloudflare-ipfs.com/ipfs/{ipfs_hash}"
+    ]
+
+    ipfs_status = []
+    for gateway in ipfs_gateways:
+        res = requests.get(gateway)
+        logging.info(res.status_code)
+        if res.status_code == 200:
+            ipfs_status.append(True)
+        else:
+            ipfs_status.append(False)
+    logging.info(f"Availability:  {ipfs_status}")
+    return True
